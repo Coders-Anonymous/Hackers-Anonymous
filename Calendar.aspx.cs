3ecb75f776667b8ed292184017ced09341803dff
KSchwarzlose95 <kmschwarzl@eagles.usi.edu>
2021-03-29 15:21:34 -0500

    Create Calendar.aspx.cs

---
+++ Calendar.aspx.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace User
+{
+    public partial class Calendar : System.Web.UI.Page
+    {
+
+        public string date_display;
+        public string due_date_display;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Calendar()
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+           
+        }
+
+    }
+}
