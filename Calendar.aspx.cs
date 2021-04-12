ed85b2c16b25b6d26b032476d34c8eb583dc4ccb
KSchwarzlose95 <kmschwarzl@eagles.usi.edu>
2021-04-11 20:08:32 -0500

    Update Calendar.aspx.cs

--- Calendar.aspx.cs
+++ Calendar.aspx.cs
@@ -4,34 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace User
 {
     public partial class Calendar : System.Web.UI.Page
     {
 
-        public string date_display;
-        public string due_date_display;
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        public string assignment;
+        public int due_date;        
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Classes.mdf;Integrated Security=True";
+            con.Open();
+            cmd.Connection = con;
 
-        }
-
-        public Calendar()
-        {
+            //GridView1.DataBind();
 
         }
 
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
-          
+
         }
 
         protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
         {
-           
+            e.Cell.Width = 15;
+            e.Cell.Height = 55;
+            e.Cell.BorderWidth = 1;
+
+           //if(e.Day.DayNumberText == "22")
+            //{
+            //    e.Cell.Controls.Add(new LiteralControl());
+           // }
+            //Calendar1.SelectedDate.ToString()
         }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+          
+        }
     }
 }
