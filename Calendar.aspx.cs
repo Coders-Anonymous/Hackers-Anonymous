ad0caffa137ebcc13c922e0b74949c0279137cf7
KSchwarzlose95 <kmschwarzl@eagles.usi.edu>
2021-04-23 18:28:42 -0500

    Revert "Update Calendar.aspx.cs"
    This reverts commit cc042587776da94e6176f4001ed40a59b4568afc.

--- Calendar.aspx.cs
+++ Calendar.aspx.cs
@@ -15,33 +15,22 @@ namespace User
         SqlCommand cmd = new SqlCommand();
         SqlDataReader dr;
 
-        //public string assignment;
-        //public int due_date;
+        public string assignment;
+        public int due_date;        
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Classes.mdf;Integrated Security=True";
-            //con.Open();
-            //cmd.Connection = con;
-
-            if (Request.QueryString["Name"] != null)
-            { Label3.Text = Request.QueryString["Name"]; }
-            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Classes.mdf;Integrated Security=True";
             con.Open();
             cmd.Connection = con;
-            cmd.CommandText = "Select StudentID, FirstName, LastName From Register Where StudentID= '" + Label3.Text + "'";
-            dr = cmd.ExecuteReader();
-            while (dr.Read())
-            {
-                Label1.Text = dr["FirstName"].ToString();
-                Label2.Text = dr["LastName"].ToString();
-            }
+
+            //GridView1.DataBind();
 
         }
 
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
-        {        
-         
+        {
+
         }
 
         protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
@@ -59,14 +48,7 @@ namespace User
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            var parameter = SqlDataSource1.InsertParameters;
-            parameter["Assignment"].DefaultValue = TextBox1.Text;
-            parameter["DueDate"].DefaultValue = Calendar1.SelectedDate.ToString();
-        }
-
-        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        {
-
+          
         }
     }
 }
